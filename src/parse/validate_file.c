5a49655a1009a0b2c42b2afa9995b482ac1229b6
Aleksander Garbacz <agarbacz@c4r1s3.42warsaw.pl>
2025-03-03 13:18:29 +0100

    dupga

--- src/parse/validate_file.c
+++ src/parse/validate_file.c
@@ -1,18 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   validate_file.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/28 11:02:41 by jzackiew          #+#    #+#             */
-/*   Updated: 2025/03/03 11:58:39 by agarbacz         ###   ########.fr       */
+/*   Created: 2025/03/03 12:01:40 by agarbacz          #+#    #+#             */
+/*   Updated: 2025/03/03 12:05:31 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/miniRT.h"
+#include "../../inc/miniRT.h"
 
-int	main(int ac, char **av)
+// returns -1 on error, 0 on cool name d-_-b
+int check_extension(char *filepath)
 {
-	prepare_window();
+    int len;
+
+	len = ft_strlen(filepath);
+	if (len < 4)
+		return (-1);
+	while (*filepath)
+	{
+		if (ft_strncmp(filepath, ".rt", 3))
+			return (0);
+		filepath++;
+	}
+	return (-1);
 }
