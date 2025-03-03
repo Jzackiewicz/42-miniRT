26512737b227edbc3ad6f7d7a56c84a92bd0db08
Aleksander Garbacz <agarbacz@c4r1s3.42warsaw.pl>
2025-03-03 14:42:06 +0100

    parser WIP

--- src/parse/validate_file.c
+++ src/parse/validate_file.c
@@ -6,25 +6,67 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:01:40 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/03 12:05:31 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/03 14:18:13 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/miniRT.h"
+#include "../../inc/parser.h"
 
 // returns -1 on error, 0 on cool name d-_-b
-int check_extension(char *filepath)
+int	check_extension(char *filepath)
 {
-    int len;
+	int	len;
 
+	if (!filepath)
+		return (-1);
 	len = ft_strlen(filepath);
 	if (len < 4)
 		return (-1);
+	if (*filepath == '.')
+		filepath++;
 	while (*filepath)
 	{
-		if (ft_strncmp(filepath, ".rt", 3))
+		if (!ft_strncmp(filepath, ".rt\0", 4))
 			return (0);
+		else if (*filepath == '.' && ft_strncmp(filepath, ".rt\0", 4))
+			return (-1);
 		filepath++;
 	}
 	return (-1);
+}
+
+// exit(1) on file error
+void	validate_file(char *filepath)
+{
+	char *line;
+	char **line_split;
+	int fd;
+
+	line = NULL;
+	if (check_extension(filepath) == -1)
+		exit(1);
+	fd = open(filepath, O_RDONLY);
+	if (fd == -1)
+		exit(1);
+	while (1)
+	{
+		line = get_next_line(fd);
+		line_split = ft_split(line, ' ');
+		if (!ft_strncmp(line_split[0], "A\0", 2))
+			validate_ambient(line_split, line);
+		else if (!ft_strncmp(line_split[0], "C\0", 2))
+			validate_camera(line_split, line);
+		else if (!ft_strncmp(line_split[0], "L\0", 2))
+			validate_light(line_split, line);
+		else if (!ft_strncmp(line_split[0], "S\0", 2))
+			validate_sphere(line_split, line);
+		else if (!ft_strncmp(line_split[0], "P\0", 2))
+			validate_plane(line_split, line);
+		else if (!ft_strncmp(line_split[0], "C\0", 2))
+			validate_cylinder(line_split[0], line);
+		else
+			exit(1);
+		ft_arr2d_free(line_split);
+		free(line);
+	}
 }
---
+++ src/parse/validate_objects.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate_objects.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 14:16:47 by agarbacz          #+#    #+#             */
+/*   Updated: 2025/03/03 14:17:01 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/parser.h"
+
+void    validate_sphere(char **line_split, char *line);
+
+void    validate_plane(char **line_split, char *line);
+
+void    validate_cylinder(char **line_split, char *line);
