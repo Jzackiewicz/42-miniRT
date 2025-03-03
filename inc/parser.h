ad10841d06d458b6f79147008b4e473f0c02b887
Aleksander Garbacz <agarbacz@c4r1s3.42warsaw.pl>
2025-03-03 18:41:12 +0100

    camera validation proto, pre tested

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:21:40 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/03 17:48:42 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:38:16 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 
 int		check_extension(char *filepath);
 int		check_colors(char *colors);
+int		check_cords(char *cords);
+int		check_vector(char *vector);
+int		is_valid_number(const char *str);
 void	validate_ambient(char **line_split, char *line);
+void	validate_camera(char **line_split, char *line);
 
 #endif
--- src/parse/validate_utils.c
+++ src/parse/validate_utils.c
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 16:19:50 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/03 17:48:02 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:38:07 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	clean_on_error(char **line_split, char *line)
 {
-    if (line)
-	    free(line);
-    if (line_split)
-	    ft_arr2d_free(line_split);
+	if (line)
+		free(line);
+	if (line_split)
+		ft_arr2d_free(line_split);
 	exit(1);
 }
 
-static int	is_valid_number(const char *str)
+int	is_valid_number(const char *str)
 {
 	int	i;
 
@@ -67,8 +67,8 @@ int	check_colors(char *colors)
 	int		i;
 	int		value;
 
-    if (!colors || colors[ft_strlen(colors) - 1] == ',')
-        return (-1);
+	if (!colors || colors[ft_strlen(colors) - 1] == ',')
+		return (-1);
 	colors_split = ft_split(colors, ',');
 	if (!colors_split)
 		return (-1);
@@ -95,5 +95,63 @@ int	check_colors(char *colors)
 		i++;
 	}
 	ft_arr2d_free(colors_split);
+	return (0);
+}
+
+int	check_cords(char *cords)
+{
+	char	**cords_split;
+	int		i;
+
+	if (!cords || cords[ft_strlen(cords) - 1] == ',')
+		return (-1);
+	cords_split = ft_split(cords, ',');
+	if (!cords_split)
+		return (-1);
+	if (ft_2d_strlen(cords_split) != 3)
+	{
+		ft_arr2d_free(cords_split);
+		return (-1);
+	}
+	i = 0;
+	while (i < 3)
+	{
+		if (!is_valid_float(cords_split[i]))
+		{
+			ft_arr2d_free(cords_split);
+			return (-1);
+		}
+		i++;
+	}
+	ft_arr2d_free(cords_split);
+	return (0);
+}
+
+int	check_vector(char *vector)
+{
+	char	**vector_split;
+	int		i;
+
+	if (!vector || vector[ft_strlen(vector) - 1] == ',')
+		return (-1);
+	vector_split = ft_split(vector, ',');
+	if (!vector_split)
+		return (-1);
+	if (ft_2d_strlen(vector_split) != 3)
+	{
+		ft_arr2d_free(vector_split);
+		return (-1);
+	}
+	i = 0;
+	while (i < 3)
+	{
+		if (!is_valid_float(vector_split[i]) || ft_atof(vector_split[i]) < -1.0
+			|| ft_atof(vector_split[i]) > 1.0)
+		{
+			ft_arr2d_free(vector_split);
+			return (-1);
+		}
+	}
+	ft_arr2d_free(vector_split);
 	return (0);
 }
