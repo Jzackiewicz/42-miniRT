8e4d3ea84829682f6cfc6294c7b21be33ca81ded
Aleksander Garbacz <agarbacz@c4r1s8.42warsaw.pl>
2025-03-04 15:27:58 +0100

    file validation refactor WIP

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:21:40 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/04 12:59:13 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/04 15:20:52 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@ int		check_cords(char *cords);
 int		check_vector(char *vector);
 int		is_valid_number(const char *str);
 int		is_valid_float(const char *str);
-void	validate_ambient(char **line_split, char *line);
-void	validate_camera(char **line_split, char *line);
-void	validate_light(char **line_split, char *line);
-void    validate_cylinder(char **line_split, char *line);
-void    validate_plane(char **line_split, char *line);
-void    validate_sphere(char **line_split, char *line);
-void	clean_on_error(char **line_split);
-void	validate_file(char *filepath);
+int     drain_file(int fd);
+int	    validate_settings(char **line_split, int fd);
+int     validate_objects(char **line_split, int fd);
+int     clean_on_error(char **line_split);
+int     validate_file(char *filepath);
 
 #endif
