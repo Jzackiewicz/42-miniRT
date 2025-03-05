103a343908bfb37b7a9a254965ca702c4da0c613
Aleksander Garbacz <agarbacz@c4r1s8.42warsaw.pl>
2025-03-05 19:15:27 +0100

    validation done and norm-ready, pre-tested

--- inc/parser.h
+++ inc/parser.h
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 13:21:40 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/04 15:20:52 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/05 19:03:34 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,30 @@
 # include "../lib/libft/libft.h"
 # include "miniRT.h"
 
-int		check_extension(char *filepath);
+// array indexes of each id type for presence checks
+# define AMBIENT 0
+# define CAMERA 1
+# define LIGHT 2
+# define SPHERE 3
+# define PLANE 4
+# define CYLINDER 5
+
+typedef struct s_key_presence
+{
+	int	no_present[6];
+}		t_key_presence;
+
 int		check_colors(char *colors);
 int		check_cords(char *cords);
-int		check_vector(char *vector);
+int		check_vector(char *vector, int i);
 int		is_valid_number(const char *str);
 int		is_valid_float(const char *str);
-int     drain_file(int fd);
-int	    validate_settings(char **line_split, int fd);
-int     validate_objects(char **line_split, int fd);
-int     clean_on_error(char **line_split);
-int     validate_file(char *filepath);
+int		drain_file(int fd);
+int		validate_settings(char **line_split, int fd,
+			t_key_presence *key_presence);
+int		validate_objects(char **line_split, int fd,
+			t_key_presence *key_presence);
+int		clean_on_error(char **line_split);
+int		validate_file(char *filepath);
 
 #endif
--- src/parse/validate_file.c
+++ src/parse/validate_file.c
@@ -6,14 +6,14 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:01:40 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/04 15:25:44 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/05 19:11:29 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/parser.h"
 
 // returns -1 on error, 0 on cool name d-_-b
-int	check_extension(char *filepath)
+static int	check_extension(char *filepath)
 {
 	int	len;
 
@@ -35,59 +35,77 @@ int	check_extension(char *filepath)
 	return (-1);
 }
 
-int drain_file(int fd)
+static void	overwrite_nl(char *line)
 {
-	char	*line;
+	if (line && line[ft_strlen(line) - 1] == '\n')
+		line[ft_strlen(line) - 1] = '\0';
+}
+
+static int	handle_empty_line(char *line)
+{
+	if (!*line)
+	{
+		free(line);
+		return (1);
+	}
+	return (0);
+}
+
+static int	read_and_validate(char *line, char **line_split, int fd,
+		t_key_presence *key_presence)
+{
+	int	objects_status;
+	int	settings_status;
 
 	while (1)
 	{
 		line = get_next_line(fd);
 		if (!line)
-			return (-1);
+			return (0);
+		overwrite_nl(line);
+		if (handle_empty_line(line))
+			continue ;
+		line_split = ft_split(line, ' ');
 		free(line);
+		settings_status = validate_settings(line_split, fd, key_presence);
+		if (settings_status == -1)
+			return (ft_arr2d_free(line_split), -1);
+		objects_status = validate_objects(line_split, fd, key_presence);
+		if (objects_status == -1)
+			return (ft_arr2d_free(line_split), -1);
+		if (settings_status == 1 && objects_status == 1)
+			return (ft_arr2d_free(line_split), drain_file(fd));
+		ft_arr2d_free(line_split);
 	}
-	close(fd);
-	return (-1);
+	return (0);
 }
 
-// exit(1) on file error
+// returns -1 on file error and exits
 // TODO: no repeats on letter presence
-// refactor
 int	validate_file(char *filepath)
 {
-	char	*line;
-	char	**line_split;
-	int		fd;
-	int		setting_status, objects_status;
+	char			*line;
+	char			**line_split;
+	int				fd;
+	t_key_presence	key_presence;
+	int				i;
 
 	line = NULL;
+	line_split = NULL;
+	ft_memset(&key_presence.no_present, 0, sizeof(key_presence.no_present));
 	if (check_extension(filepath) == -1)
-		exit(1);
+		return (-1);
 	fd = open(filepath, O_RDONLY);
 	if (fd == -1)
 		exit(1);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (line && line[ft_strlen(line) - 1] == '\n')
-			line[ft_strlen(line) - 1] = '\0';
-		if (line)
-		{
-			line_split = ft_split(line, ' ');
-			free(line);
-		}
-		else
-			break ;
-		setting_status = validate_settings(line_split, fd);
-		if (setting_status == -1)
-			return (-1);
-		objects_status = validate_objects(line_split, fd);
-		if (objects_status == -1)
-			return (-1);
-		if (setting_status == 1 && objects_status == 1)
-			return (drain_file(fd));
-		ft_arr2d_free(line_split);
-	}
+	if (read_and_validate(line, line_split, fd, &key_presence) != 0)
+		return (-1);
 	close(fd);
+	i = -1;
+	while (++i < 6)
+	{
+		if (key_presence.no_present[i] != 1)
+			return (-1);
+	}
 	return (0);
 }
