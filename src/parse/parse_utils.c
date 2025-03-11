6a3abf1a4e6076d610040784bcb42a4b79fc48e2
Aleksander Garbacz <agarbacz@c1r7s1.42warsaw.pl>
2025-03-11 14:43:42 +0100

    parsing fixes + tests WIP

--- src/parse/parse_utils.c
+++ src/parse/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:43:53 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/03/11 13:26:01 by agarbacz         ###   ########.fr       */
+/*   Updated: 2025/03/11 14:10:31 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,48 +75,31 @@ void	parse_cords(char *cords, t_object *obj)
 	ft_arr2d_free(cords_split);
 }
 
-void	free_object(t_object *obj)
+void	print_object(t_object *obj)
 {
-	if (obj)
+	if (!obj)
 	{
-		if (obj->id)
-			free(obj->id);
-		if (obj->orientation_vector)
-			free(obj->orientation_vector);
-		if (obj->colors)
-			free(obj->colors);
-		if (obj->cords)
-			free(obj->cords);
-		if (obj->vector)
-			free(obj->vector);
+		printf("Error: Object is NULL\n");
+		return ;
 	}
+	if (obj->id)
+		printf("id: %s\n", obj->id);
+	printf("ambient_light_ratio: %f\n", obj->ambient_light_ratio);
+	if (obj->orientation_vector)
+		printf("orientation_vector: %f %f %f\n", obj->orientation_vector[0],
+			obj->orientation_vector[1], obj->orientation_vector[2]);
+	printf("fov: %d\n", obj->fov);
+	printf("brightness: %f\n", obj->brightness);
+	printf("diameter: %f\n", obj->diameter);
+	printf("height: %f\n", obj->height);
+	if (obj->colors)
+		printf("colors: %d %d %d\n", obj->colors[0], obj->colors[1],
+			obj->colors[2]);
+	if (obj->cords)
+		printf("cords: %f %f %f\n", obj->cords[0], obj->cords[1],
+			obj->cords[2]);
+	if (obj->vector)
+		printf("vector: %f %f %f\n", obj->vector[0], obj->vector[1],
+			obj->vector[2]);
+	printf("height: %f\n", obj->height);
 }
-
-// void	print_object(t_object *obj)
-// {
-// 	if (!obj)
-// 	{
-// 		printf("Error: Object is NULL\n");
-// 		return ;
-// 	}
-// 	if (obj->id)
-// 		printf("id: %s\n", obj->id);
-// 	printf("ambient_light_ratio: %f\n", obj->ambient_light_ratio);
-// 	if (obj->orientation_vector)
-// 		printf("orientation_vector: %f %f %f\n", obj->orientation_vector[0],
-// 			obj->orientation_vector[1], obj->orientation_vector[2]);
-// 	printf("fov: %d\n", obj->fov);
-// 	printf("brightness: %f\n", obj->brightness);
-// 	printf("diameter: %f\n", obj->diameter);
-// 	printf("height: %f\n", obj->height);
-// 	if (obj->colors)
-// 		printf("colors: %d %d %d\n", obj->colors[0], obj->colors[1],
-// 			obj->colors[2]);
-// 	if (obj->cords)
-// 		printf("cords: %f %f %f\n", obj->cords[0], obj->cords[1],
-// 			obj->cords[2]);
-// 	if (obj->vector)
-// 		printf("vector: %f %f %f\n", obj->vector[0], obj->vector[1],
-// 			obj->vector[2]);
-// 	printf("height: %f\n", obj->height);
-// }
