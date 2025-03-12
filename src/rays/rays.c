5890b58d9d09e768362e226025e88028c8212348
jzackiew <jzackiew@student.42warsaw.pl>
2025-03-12 15:09:43 +0100

    ray-sphere intersections

---
+++ src/rays/rays.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rays.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 13:32:48 by jzackiew          #+#    #+#             */
+/*   Updated: 2025/03/12 15:00:14 by jzackiew         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/rays.h"
+
+t_ray	*create_ray(double *origin, double *direction)
+{
+	t_ray	*new_ray;
+
+	new_ray = (t_ray *)malloc(sizeof(t_ray));
+	if (!new_ray)
+		return (NULL);
+	new_ray->origin = origin;
+	new_ray->direction = direction;
+	new_ray->t = 0;
+	return (new_ray);
+}
+
+
+double	*position(t_ray *ray, double t)
+{
+	double	*dir_t;
+	double	*pos;
+	
+	dir_t = multiply_tuple(ray->direction, t);
+	pos = add_tuple(ray->origin, dir_t);
+	free(dir_t);
+	
+	return (pos);
+}
+
