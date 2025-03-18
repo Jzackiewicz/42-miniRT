41a4933c3468d507958adec82c74b10d383b5671
Aleksander Garbacz <agarbacz@c4r1s2.42warsaw.pl>
2025-03-18 12:42:48 +0100

    refactor, leak fixes WIP

---
+++ src/graphics/mlx_manage/run_mlx.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   run_mlx.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/13 16:15:56 by agarbacz          #+#    #+#             */
+/*   Updated: 2025/03/18 11:49:52 by agarbacz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../inc/graphics.h"
+
+void	init_mlx(t_ray_tracer_data **rt_data)
+{
+	*rt_data = malloc(sizeof(t_ray_tracer_data));
+	if (!(*rt_data))
+		exit(1);
+	(*rt_data)->mlx_data = malloc(sizeof(t_mlx_data));
+	if (!(*rt_data)->mlx_data)
+		exit(1);
+	(*rt_data)->mlx_data->mlx_ptr = mlx_init();
+	(*rt_data)->mlx_data->window_ptr = mlx_new_window((*rt_data)->mlx_data->mlx_ptr,
+			WINDOW_WIDTH, WINDOW_HEIGHT, "miniRT");
+	(*rt_data)->mlx_data->img_ptr = mlx_new_image((*rt_data)->mlx_data->mlx_ptr,
+			WINDOW_WIDTH, WINDOW_HEIGHT);
+	(*rt_data)->mlx_data->img_addr = mlx_get_data_addr((*rt_data)->mlx_data->img_ptr,
+			&(*rt_data)->mlx_data->bits_per_pixel,
+			&(*rt_data)->mlx_data->line_length, &(*rt_data)->mlx_data->endian);
+}
+
+void	mlx_run(int **bitmap)
+{
+	t_ray_tracer_data	*rt_data;
+
+	rt_data = NULL;
+	init_mlx(&rt_data);
+	render_image(rt_data, bitmap);
+	mlx_key_hook(rt_data->mlx_data->window_ptr, handle_key_input, rt_data);
+	mlx_hook(rt_data->mlx_data->window_ptr, 17, 0, close_window, rt_data);
+	mlx_loop(rt_data->mlx_data->mlx_ptr);
+}
--- src/rays/casting_rays.c
+++ src/rays/casting_rays.c
@@ -3,43 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   casting_rays.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
+/*   By: agarbacz <agarbacz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:32:48 by jzackiew          #+#    #+#             */
-/*   Updated: 2025/03/18 10:03:05 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/03/18 12:41:24 by agarbacz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/rays.h"
 #include "../../inc/miniRT.h"
+#include "../../inc/rays.h"
 
-double	get_canvas_width(t_camera *cam_data)
-{
-	double	distance;
-	double	*cam_to_center;
-	double	center[4];
-	double	angle;
-	double	width;
-	
-	center[0] = 0.0;
-	center[1] = 0.0;
-	center[2] = 0.0;
-	center[3] = 0.0;
-	cam_to_center = subtract_tuple(cam_data->origin, center);
-	distance = magnitude(cam_to_center);
-	angle = cam_data->fov * (M_PI / 180.0);
-	width = 2 * (tan(angle / 2) * distance);
-	return (width);
-}
 /* Not sure if works */
-double	*get_cam_orientation_vector(t_camera *cam_data, int h_id, int w_id)
+static double	*get_cam_orientation_vector(t_camera *cam_data, int h_id, int w_id)
 {
-	double	width;
-	double	angle[2];
-	double	*direction;	
-	double	ratio;
+	double		angle[2];
+	double		*direction;
+	double		ratio;
 	t_matrix	*tmp;
-	
+
 	angle[0] = cam_data->fov * (M_PI / 180) / 2;
 	ratio = (double)WINDOW_WIDTH / (double)WINDOW_HEIGHT;
 	angle[1] = angle[0] * ratio;
@@ -54,27 +35,41 @@ double	*get_cam_orientation_vector(t_camera *cam_data, int h_id, int w_id)
 	return (direction);
 }
 
+t_ray	*create_ray(double *origin, double *direction)
+{
+	t_ray	*new_ray;
+
+	new_ray = (t_ray *)malloc(sizeof(t_ray));
+	if (!new_ray)
+		return (NULL);
+	new_ray->origin = origin;
+	new_ray->direction = direction;
+	return (new_ray);
+}
+
 t_ray	**generate_rays(t_camera *cam_data)
 {
-	int	i;
-	int	j;
-	double	width;
+	int		i;
+	int		j;
 	double	*direction;
+	// double	*tmp;
 	t_ray	**rays;
-	
-	width = get_canvas_width(cam_data);
-	rays = (t_ray **)ft_calloc(WINDOW_WIDTH * WINDOW_HEIGHT, sizeof(t_ray *));
+
+	rays = (t_ray **)ft_calloc(WINDOW_WIDTH * WINDOW_HEIGHT + 1, sizeof(t_ray *));
 	i = -1;
 	while (++i < WINDOW_HEIGHT)
 	{
 		j = -1;
 		while (++j < WINDOW_WIDTH)
 		{
-			rays[i * WINDOW_WIDTH + j] = (t_ray *)malloc (sizeof(t_ray));
-			if (!rays[i * WINDOW_WIDTH + j])
-				return (NULL);
+			// rays[i * WINDOW_WIDTH + j] = (t_ray *)malloc(sizeof(t_ray));
+			// if (!rays[i * WINDOW_WIDTH + j])
+			// 	return (NULL);
 			direction = get_cam_orientation_vector(cam_data, i, j);
-			rays[i * WINDOW_WIDTH + j] = create_ray(cam_data->origin, direction);
+			// free(tmp);
+			// direction = normalize(tmp);
+			rays[i * WINDOW_WIDTH + j] = create_ray(cam_data->origin,
+					direction);
 		}
 	}
 	return (rays);
