becc96701cbbecc28947f68cb3e1d4276ba36ad4
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-04-14 17:17:13 +0200

    make planes not cast a shadow

--- src/rays/lighting/shadows.c
+++ src/rays/lighting/shadows.c
@@ -6,7 +6,7 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 12:23:41 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/04/11 16:22:39 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:37:41 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ bool	is_shadowed(t_world *w, double *worldp)
 	normalize(&lightv);
 	hit = precompute_shadows(worldp, lightv, w->objs);
 	free(lightv);
-	if (hit && hit->t < distance)
+	if (hit && hit->t < distance && ft_strncmp(hit->object->id, "pl\0", 3))
 		return (free(hit), true);
 	return (false);
 }
