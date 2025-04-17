1531c1bf2515e2cd2f13f034e33bed1194edb86e
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-04-17 17:11:39 +0200

    cylinder with truncated caps

--- src/rays/intersections/intersect_cylinder.c
+++ src/rays/intersections/intersect_cylinder.c
@@ -6,26 +6,24 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 10:09:22 by jzackiew          #+#    #+#             */
-/*   Updated: 2025/04/17 15:31:13 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:10:38 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/miniRT.h"
 #include "../../../inc/rays.h"
 
-static double	*truncate_cylinder(double *arr_t, t_ray *ray, t_object *obj)
+static double	*truncate_cylinder(double *arr_t, t_ray *ray, double cyl_height)
 {
-	double	limit;
 	bool	is_t[2];
 	int		i;
 	double	*out_t;
 
-	limit = obj->height / 2;
 	i = -1;
 	while (++i < 2)
 	{
 		if (ray->origin[1] + arr_t[i] * ray->direction[1] > 0 && ray->origin[1]
-			+ arr_t[i] * ray->direction[1] < obj->height)
+			+ arr_t[i] * ray->direction[1] < cyl_height)
 			is_t[i] = true;
 		else
 			is_t[i] = false;
@@ -41,7 +39,7 @@ static double	*truncate_cylinder(double *arr_t, t_ray *ray, t_object *obj)
 	return (out_t);
 }
 
-double	*truncate_caps(t_ray *ray, double *arr_t, t_object *obj)
+static double	*truncate_caps(t_ray *ray, double *arr_t, double diameter)
 {
 	int		i;
 	double	x;
@@ -49,30 +47,28 @@ double	*truncate_caps(t_ray *ray, double *arr_t, t_object *obj)
 	double	is_t[2];
 	double	*out_t;
 
-	(void)obj;
 	i = -1;
 	while (++i < 2)
 	{
 		x = ray->origin[0] + arr_t[i] * ray->direction[0];
 		z = ray->origin[2] + arr_t[i] * ray->direction[2];
-		if (pow(x, 2) + pow(z, 2) < 1 || compare_floats(pow(x, 2) + pow(z, 2),
-				1))
+		if (pow(x, 2) + pow(z, 2) < diameter || compare_floats(pow(x, 2)
+				+ pow(z, 2), diameter))
 			is_t[i] = true;
 		else
 			is_t[i] = false;
 	}
 	out_t = tupledup(arr_t, 2);
 	if (!is_t[0] && !is_t[1])
-		return (free(arr_t), NULL);
-	// if (is_t[0] && !is_t[1])
-	// 	out_t[1] = arr_t[0];
-	// else if (is_t[1] && !is_t[0])
-	// 	out_t[0] = arr_t[1];
-	free(arr_t);
-	return (out_t);
+		return (free(arr_t), free(out_t), NULL);
+	if (is_t[0] && !is_t[1])
+		out_t[1] = arr_t[0];
+	else if (is_t[1] && !is_t[0])
+		out_t[0] = arr_t[1];
+	return (free(arr_t), out_t);
 }
 
-double	*intersect_caps(double *arr_t, t_ray *ray, double cyl_height)
+static double	*intersect_caps(double *arr_t, t_ray *ray, double cyl_height)
 {
 	double	*out_t;
 	double	limit;
@@ -84,20 +80,19 @@ double	*intersect_caps(double *arr_t, t_ray *ray, double cyl_height)
 	out_t = (double *)malloc(sizeof(double) * 2);
 	out_t[0] = -ray->origin[1] / ray->direction[1];
 	out_t[1] = (cyl_height - ray->origin[1]) / ray->direction[1];
-	if (!arr_t)
-		return (free(arr_t), out_t);
-	// out_t = truncate_caps(ray, out_t, obj);
-	// if (!out_t)
-	// 	return (free(out_t), arr_t);
-	if (ray->direction[0] > 0)
+	if (arr_t && arr_t[0] < out_t[0])
 		out_t[0] = arr_t[0];
-	else
+	if (arr_t && arr_t[1] < out_t[1])
 		out_t[1] = arr_t[1];
+	if (arr_t && arr_t[1] < out_t[0])
+		out_t[0] = arr_t[1];
+	if (arr_t && arr_t[0] < out_t[1])
+		out_t[1] = arr_t[0];
 	free(arr_t);
 	return (out_t);
 }
 
-static double	*intersect_infinite_tube(t_ray *ray, double width)
+static double	*intersect_infinite_tube(t_ray *ray, double diameter)
 {
 	double	coefficients[3];
 	double	delta;
@@ -108,7 +103,8 @@ static double	*intersect_infinite_tube(t_ray *ray, double width)
 		return (NULL);
 	coefficients[1] = 2 * ray->origin[0] * ray->direction[0] + 2
 		* ray->origin[2] * ray->direction[2];
-	coefficients[2] = pow(ray->origin[0], 2) + pow(ray->origin[2], 2) - width;
+	coefficients[2] = pow(ray->origin[0], 2) + pow(ray->origin[2], 2)
+		- diameter;
 	delta = pow(coefficients[1], 2) - 4 * coefficients[0] * coefficients[2];
 	if (delta < 0)
 		return (NULL);
@@ -129,7 +125,9 @@ double	*intersect_cylinder(t_ray *ray, t_object *obj)
 
 	arr_t = intersect_infinite_tube(ray, obj->diameter);
 	if (arr_t)
-		arr_t = truncate_cylinder(arr_t, ray, obj);
+		arr_t = truncate_cylinder(arr_t, ray, obj->height);
 	arr_t = intersect_caps(arr_t, ray, obj->height);
+	if (arr_t)
+		arr_t = truncate_caps(ray, arr_t, obj->diameter);
 	return (arr_t);
 }
