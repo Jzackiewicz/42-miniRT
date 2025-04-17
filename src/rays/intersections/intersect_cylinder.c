494eabd85a2d138284d76bf512f5d28ae38ef5ad
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-04-17 15:32:35 +0200

    cylinder with untruncated caps

---
+++ src/rays/intersections/intersect_cylinder.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   intersect_cylinder.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 10:09:22 by jzackiew          #+#    #+#             */
+/*   Updated: 2025/04/17 15:31:13 by jzackiew         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../inc/miniRT.h"
+#include "../../../inc/rays.h"
+
+static double	*truncate_cylinder(double *arr_t, t_ray *ray, t_object *obj)
+{
+	double	limit;
+	bool	is_t[2];
+	int		i;
+	double	*out_t;
+
+	limit = obj->height / 2;
+	i = -1;
+	while (++i < 2)
+	{
+		if (ray->origin[1] + arr_t[i] * ray->direction[1] > 0 && ray->origin[1]
+			+ arr_t[i] * ray->direction[1] < obj->height)
+			is_t[i] = true;
+		else
+			is_t[i] = false;
+	}
+	if (!is_t[0] && !is_t[1])
+		return (free(arr_t), NULL);
+	out_t = tupledup(arr_t, 2);
+	if (is_t[0] && !is_t[1])
+		out_t[1] = arr_t[0];
+	else if (is_t[1] && !is_t[0])
+		out_t[0] = arr_t[1];
+	free(arr_t);
+	return (out_t);
+}
+
+double	*truncate_caps(t_ray *ray, double *arr_t, t_object *obj)
+{
+	int		i;
+	double	x;
+	double	z;
+	double	is_t[2];
+	double	*out_t;
+
+	(void)obj;
+	i = -1;
+	while (++i < 2)
+	{
+		x = ray->origin[0] + arr_t[i] * ray->direction[0];
+		z = ray->origin[2] + arr_t[i] * ray->direction[2];
+		if (pow(x, 2) + pow(z, 2) < 1 || compare_floats(pow(x, 2) + pow(z, 2),
+				1))
+			is_t[i] = true;
+		else
+			is_t[i] = false;
+	}
+	out_t = tupledup(arr_t, 2);
+	if (!is_t[0] && !is_t[1])
+		return (free(arr_t), NULL);
+	// if (is_t[0] && !is_t[1])
+	// 	out_t[1] = arr_t[0];
+	// else if (is_t[1] && !is_t[0])
+	// 	out_t[0] = arr_t[1];
+	free(arr_t);
+	return (out_t);
+}
+
+double	*intersect_caps(double *arr_t, t_ray *ray, double cyl_height)
+{
+	double	*out_t;
+	double	limit;
+
+	if (compare_floats(ray->direction[1], 0) || (arr_t
+			&& !compare_floats(arr_t[0], arr_t[1])))
+		return (arr_t);
+	limit = cyl_height / 2;
+	out_t = (double *)malloc(sizeof(double) * 2);
+	out_t[0] = -ray->origin[1] / ray->direction[1];
+	out_t[1] = (cyl_height - ray->origin[1]) / ray->direction[1];
+	if (!arr_t)
+		return (free(arr_t), out_t);
+	// out_t = truncate_caps(ray, out_t, obj);
+	// if (!out_t)
+	// 	return (free(out_t), arr_t);
+	if (ray->direction[0] > 0)
+		out_t[0] = arr_t[0];
+	else
+		out_t[1] = arr_t[1];
+	free(arr_t);
+	return (out_t);
+}
+
+static double	*intersect_infinite_tube(t_ray *ray, double width)
+{
+	double	coefficients[3];
+	double	delta;
+	double	*arr_t;
+
+	coefficients[0] = pow(ray->direction[0], 2) + pow(ray->direction[2], 2);
+	if (compare_floats(coefficients[0], 0))
+		return (NULL);
+	coefficients[1] = 2 * ray->origin[0] * ray->direction[0] + 2
+		* ray->origin[2] * ray->direction[2];
+	coefficients[2] = pow(ray->origin[0], 2) + pow(ray->origin[2], 2) - width;
+	delta = pow(coefficients[1], 2) - 4 * coefficients[0] * coefficients[2];
+	if (delta < 0)
+		return (NULL);
+	arr_t = (double *)malloc(sizeof(double) * 2);
+	if (!arr_t)
+		return (NULL);
+	arr_t[0] = (-coefficients[1] - sqrt(delta)) / (2 * coefficients[0]);
+	arr_t[1] = (-coefficients[1] + sqrt(delta)) / (2 * coefficients[0]);
+	return (arr_t);
+}
+
+/* Finds t for ray-cylinder intersection for a closed cylinder.
+Intersected cylinder is a cylinder that is already translated and scaled by
+parameters in obj argument.*/
+double	*intersect_cylinder(t_ray *ray, t_object *obj)
+{
+	double	*arr_t;
+
+	arr_t = intersect_infinite_tube(ray, obj->diameter);
+	if (arr_t)
+		arr_t = truncate_cylinder(arr_t, ray, obj);
+	arr_t = intersect_caps(arr_t, ray, obj->height);
+	return (arr_t);
+}
--- src/rays/intersections/intersect_objects.c
+++ src/rays/intersections/intersect_objects.c
@@ -6,7 +6,7 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:34:26 by jzackiew          #+#    #+#             */
-/*   Updated: 2025/04/16 20:40:02 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/04/17 10:15:32 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,110 +59,6 @@ static double	*intersect_plane(t_ray *ray)
 	return (arr_t);
 }
 
-double	*truncate_cylinder(double *arr_t, t_ray *ray, t_object *obj)
-{
-	double	limit;
-	bool	is_t[2];
-	int		i;
-	double	*out_t;
-
-	limit = obj->height / 2;
-	i = -1;
-	while (++i < 2)
-	{
-		if (ray->origin[1] + arr_t[i] * ray->direction[1] > obj->coords[1] - limit &&
-			ray->origin[1] + arr_t[i] * ray->direction[1] < obj->coords[1] + limit)
-				is_t[i] = true;
-		else
-			is_t[i] = false;
-	}
-	if (!is_t[0] && !is_t[1])
-		return(free(arr_t), NULL);
-	out_t = tupledup(arr_t, 2);
-	if (is_t[0] && !is_t[1])
-		out_t[1] = arr_t[0];
-	else if (is_t[1] && !is_t[0])
-		out_t[0] = arr_t[1];
-	free(arr_t);
-	return(out_t);
-}
-
-/* Finds t for ray-cylinder intersection for identity cylinder.
-Identity cylinder is a cylinder that has it's center at (0, 0, 0) world point,
-is infinitely long in +y and -y directions and it's radius length is 1*/
-static	double	*intersect_cylinder(t_ray *ray)
-{
-	double	coefficients[3];
-	double	delta;
-	double	*arr_t;
-
-	coefficients[0] = pow(ray->direction[0], 2) + pow(ray->direction[2], 2);
-	if (compare_floats(coefficients[0], 0))
-		return (NULL);
-	coefficients[1] = 2 * ray->origin[0] * ray->direction[0] + 2 * ray->origin[2] * ray->direction[2];
-	coefficients[2] = pow(ray->origin[0], 2) + pow(ray->origin[2], 2) - 1;
-	delta = pow(coefficients[1], 2) - 4 * coefficients[0] * coefficients[2];
-	if (delta < 0)
-		return (NULL);
-	arr_t = (double *)malloc(sizeof(double) * 2);
-	if (!arr_t)
-		return (NULL);
-	arr_t[0] = (-coefficients[1] - sqrt(delta)) / (2 * coefficients[0]);
-	arr_t[1] = (-coefficients[1] + sqrt(delta)) / (2 * coefficients[0]);
-	return (arr_t);
-}
-
-double	*truncate_caps(t_ray *ray, double *arr_t)
-{
-	int		i;
-	double	x;
-	double	z;
-	double	is_t[2];
-	double	*out_t;
-	
-	i = -1;
-	while (++i < 2)
-	{
-		x = ray->origin[0] + arr_t[i] * ray->direction[0];
-		z = ray->origin[2] + arr_t[i] * ray->direction[2];
-		if (pow(x, 2) + pow(z, 2) < 1 || compare_floats(pow(x, 2) + pow(z, 2), 1))
-			is_t[i] = true;
-		else
-			is_t[i] = false;
-	}
-	out_t = tupledup(arr_t, 2);
-	if (!is_t[0] && !is_t[1])
-		return(free(arr_t), NULL);
-	if (is_t[0] && !is_t[1])
-		out_t[1] = arr_t[0];
-	else if (is_t[1] && !is_t[0])
-		out_t[0] = arr_t[1];
-	free(arr_t);
-	return(out_t);
-}
-
-double	*intersect_caps(double *arr_t, t_ray *ray, t_object *obj)
-{
-	double	*out_t;
-	double	limit;
-	
-	if (compare_floats(ray->direction[1], 0) || (arr_t && !compare_floats(arr_t[0], arr_t[1])))
-		return (arr_t);
-	limit = obj->height / 2;
-	out_t = (double *)malloc(sizeof(double) * 2);
-	out_t[0] = (obj->coords[1] - limit - ray->origin[1]) / ray->direction[1];
-	out_t[1] = (obj->coords[1] + limit - ray->origin[1]) / ray->direction[1];
-	if (!arr_t)
-		return (free(arr_t), out_t);
-	out_t = truncate_caps(ray, out_t);
-	if (ray->direction[0] > 0)
-		out_t[0] = arr_t[0];
-	else
-		out_t[1] = arr_t[0];
-	free(arr_t);
-	return(out_t);
-}
-
 /* highest level intersection finding function
 	called with a pointer to an object (plane, sphere or cylinder)
 	and an initialized t_ray pointer */
@@ -176,12 +72,7 @@ double	*intersect(t_object *obj, t_ray *ray)
 	else if (!ft_strncmp(obj->id, "pl\0", 3))
 		arr_t = intersect_plane(ray);
 	else if (!ft_strncmp(obj->id, "cy\0", 3))
-	{
-		arr_t = intersect_cylinder(ray);
-		if (arr_t)
-			arr_t = truncate_cylinder(arr_t, ray, obj);
-		arr_t = intersect_caps(arr_t, ray, obj);
-	}
+		arr_t = intersect_cylinder(ray, obj);
 	else
 	{
 		printf("Error: unknown object id: %s\n", obj->id);
