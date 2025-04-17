c57216d0d8928fbeda55800a5e2cd7bafd903087
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-04-17 18:02:53 +0200

    get cylinder normal

--- src/scene/prepare_world.c
+++ src/scene/prepare_world.c
@@ -6,19 +6,19 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 17:42:53 by agarbacz          #+#    #+#             */
-/*   Updated: 2025/04/17 17:31:37 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:47:56 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/miniRT.h"
 
-static double *get_sphere_normal_at(t_object *obj, double *w_point)
+static double	*get_sphere_normal_at(t_object *obj, double *w_point)
 {
 	double	*sphere_center;
 	double	*obj_normal;
 	double	*obj_point;
 	double	*world_normal;
-	
+
 	sphere_center = init_tuple(1);
 	obj_point = multiply_tuple_and_matrix(obj->inv_transform, w_point);
 	obj_normal = subtract_tuple(obj_point, sphere_center);
@@ -42,9 +42,11 @@ double	*get_cylinder_normal_at(t_object *obj, double *w_point)
 	distance = pow(w_point[0], 2) + pow(w_point[2], 2);
 	limit = obj->height / 2;
 	obj_normal = init_tuple(0);
-	if (distance < obj->diameter - EPSILON && (w_point[1] > obj->coords[1] + limit + EPSILON))
+	if (distance < obj->diameter - EPSILON && (w_point[1] > obj->coords[1]
+			+ limit + EPSILON))
 		obj_normal[1] = 1;
-	else if (distance < obj->diameter - EPSILON && (w_point[1] < obj->coords[1] - limit - EPSILON))
+	else if (distance < obj->diameter - EPSILON && (w_point[1] < obj->coords[1]
+			- limit - EPSILON))
 		obj_normal[1] = -1;
 	else
 	{
@@ -52,7 +54,7 @@ double	*get_cylinder_normal_at(t_object *obj, double *w_point)
 		obj_normal[2] = w_point[2];
 	}
 	normalize(&obj_normal);
-	return(obj_normal);
+	return (obj_normal);
 }
 
 /* returns the normal vector at a point on the surface of the object*/
@@ -65,13 +67,7 @@ static double	*get_normal_at(t_object *obj, double *w_point)
 	else if (!strncmp(obj->id, "sp\0", 3))
 		obj_normal = get_sphere_normal_at(obj, w_point);
 	else if (!strncmp(obj->id, "cy\0", 3))
-	{
 		obj_normal = get_cylinder_normal_at(obj, w_point);
-		// obj_normal = init_tuple(0);
-		// obj_normal[0] = w_point[0];
-		// obj_normal[2] = w_point[2];
-		// normalize(&obj_normal);
-	}
 	else
 		obj_normal = NULL;
 	return (obj_normal);
