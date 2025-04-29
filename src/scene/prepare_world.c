4ce702be9d963eead85c848231a12bec4b9d32aa
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-04-29 11:18:58 +0200

    norm fix

--- src/scene/prepare_world.c
+++ src/scene/prepare_world.c
@@ -6,7 +6,7 @@
 /*   By: jzackiew <jzackiew@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 11:14:54 by jzackiew          #+#    #+#             */
-/*   Updated: 2025/04/29 11:15:05 by jzackiew         ###   ########.fr       */
+/*   Updated: 2025/04/29 11:18:39 by jzackiew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,46 @@
 
 /* default world initializer
 	takes an array of objects and the light + ambient data */
-	t_world	*create_world(t_input_data **input_data)
+t_world	*create_world(t_input_data **input_data)
+{
+	t_world	*world;
+
+	world = (t_world *)malloc(sizeof(t_world));
+	if (!world)
+		return (NULL);
+	world->objs = get_objects(input_data);
+	world->camera = get_cam_data(input_data);
+	world->lights = get_light_data(input_data);
+	world->ambient = get_ambient_data(input_data);
+	free_input_ids(input_data);
+	return (world);
+}
+
+t_comps	*prepare_computations(t_intersec *intersection, t_ray *ray)
+{
+	t_comps	*comps;
+	double	*tmp;
+	double	*normal_epsilon_product;
+
+	comps = malloc(sizeof(t_comps));
+	if (!comps)
+		return (NULL);
+	comps->t = intersection->t;
+	comps->obj = intersection->object;
+	comps->point = position(ray, comps->t);
+	comps->eyev = negate_tuple(ray->direction);
+	comps->normalv = get_normal_at(comps->obj, comps->point);
+	normal_epsilon_product = multiply_tuple(comps->normalv, EPSILON);
+	comps->over_point = add_tuple(comps->point, normal_epsilon_product);
+	free(normal_epsilon_product);
+	if (dot(comps->normalv, comps->eyev) < 0)
 	{
-		t_world	*world;
-	
-		world = (t_world *)malloc(sizeof(t_world));
-		if (!world)
-			return (NULL);
-		world->objs = get_objects(input_data);
-		world->camera = get_cam_data(input_data);
-		world->lights = get_light_data(input_data);
-		world->ambient = get_ambient_data(input_data);
-		free_input_ids(input_data);
-		return (world);
+		comps->inside = true;
+		tmp = comps->normalv;
+		comps->normalv = negate_tuple(tmp);
+		free(tmp);
 	}
-	
-	t_comps	*prepare_computations(t_intersec *intersection, t_ray *ray)
-	{
-		t_comps	*comps;
-		double	*tmp;
-		double	*normal_epsilon_product;
-	
-		comps = malloc(sizeof(t_comps));
-		if (!comps)
-			return (NULL);
-		comps->t = intersection->t;
-		comps->obj = intersection->object;
-		comps->point = position(ray, comps->t);
-		comps->eyev = negate_tuple(ray->direction);
-		comps->normalv = get_normal_at(comps->obj, comps->point);
-		normal_epsilon_product = multiply_tuple(comps->normalv, EPSILON);
-		comps->over_point = add_tuple(comps->point, normal_epsilon_product);
-		free(normal_epsilon_product);
-		if (dot(comps->normalv, comps->eyev) < 0)
-		{
-			comps->inside = true;
-			tmp = comps->normalv;
-			comps->normalv = negate_tuple(tmp);
-			free(tmp);
-		}
-		else
-			comps->inside = false;
-		return (comps);
-	}
+	else
+		comps->inside = false;
+	return (comps);
+}
